c4bce18961ce1ebefecd0b0bb786ac3d1351f7d4
uzaif313 <uzaif313@gmail.com>
2016-10-06 09:23:23 +0530

    added redux-promise and axios module

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,12 @@
+import axios from 'axios'
 const API_KEY = 'f0bca41afd511e6a123ce6d62bf8a45a'
-
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`
+export FEATCH_WEATHER = 'FEATCH_WEATHER';
+export function featchWether(city){
+	const url = `${ROOT_URL}&q=${city},us`
+	const request = axios.get(url)
+	return{
+		type:FEATCH_WEATHER,
+		payload:request
+	}
+}
